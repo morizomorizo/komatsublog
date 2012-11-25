50f9b1f6c670ea2778bcc769b8516c8eaa0dd13a
ubuntu <higuzo@ubuntu.(none)>
2012-11-25 11:25:56 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Komatsublog::Application.config.secret_token = 'd0e1c01723be0142194906f635fc667a924d7dc743ae09895692fe359f4cfadf81e94a7b3b35cb16f4695dab0e82e559c3d3cbb720646ba53d0fba7514477cba'
